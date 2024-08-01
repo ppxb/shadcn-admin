110f06eb54900ee518d91ccb650cec02d56783ad
ppxb <317842449@qq.com>
2024-08-01 10:35:47 +0800

    dev: format code

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import path from 'node:path'
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-const alias={
-  "~":path.resolve(__dirname,".","src")
+const alias = {
+  '~': path.resolve(__dirname, '.', 'src')
 }
 
 export default defineConfig({
   plugins: [react()],
-  resolve:{
+  resolve: {
     alias
   }
 })
